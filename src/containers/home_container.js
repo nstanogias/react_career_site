711ad060fd825c3eb20b0e82dbe0302706879912
Nikos Stanogias <nstanogias@gmail.com>
2018-05-04 00:19:37 +0300

    added container

---
+++ src/containers/home_container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getJobAdds } from '../actions/actions';
+
+class HomeContainer extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default HomeContainer;
