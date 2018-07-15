ee164388a1c24435b9e0f2fde5aa7901dd67e1b1
Nikos Stanogias <nstanogias@gmail.com>
2018-07-15 19:16:55 +0200

    prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/user');
 const jobs = require('./routes/api/job');
@@ -33,7 +34,14 @@ app.use('/api/jobs', jobs);
 app.use('/api/userjobs', userjobs);
 app.use('/api/profile', profile);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'));
